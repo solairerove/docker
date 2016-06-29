9c5c6a6e8e8f9054e4ed0d4268887d5c9ae59a5c
Krivitski Nikita <hax2033@gmail.com>
2016-06-29 22:36:43 +0300

    update-alternatives for java and javac

--- shell/setup.sh
+++ shell/setup.sh
@@ -2,12 +2,19 @@
 
 # install jdk-8u92
 
-#wget --no-check-certificate --no-cookies --header \
-#"Cookie: oraclelicense=accept-securebackup-cookie" \
-#http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies --header \
+"Cookie: oraclelicense=accept-securebackup-cookie" \
+http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
 
-#tar -xf jdk-8u92-linux-x64.tar.gz -C /usr/lib/jvm/
+tar -xf jdk-8u92-linux-x64.tar.gz -C /usr/lib/jvm/
 #rm jdk-8u92-linux-x64.tar.gz
+
+update-alternatives --remove-all javac
+update-alternatives --remove-all java
+update-alternatives --set /usr/bin/javac javac /usr/lib/jvm/jdk1.8.0_92/bin/javac 1
+update-alternatives --set /usr/bin/java java /usr/lib/jvm/jdk1.8.0_92/bin/java 1
+update-alternatives --config javac
+update-alternatives --config java
 
 # install maven 3.3.9
 
@@ -18,8 +25,8 @@
 
 # install tomcat 8.0.36
 
-wget --quiet --no-cookies \
-http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz
+#wget --quiet --no-cookies \
+#http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz
 
 #tar -xf apache-tomcat-8.0.36.tar.gz -C /opt/
 #rm apache-tomcat-8.0.36.tar.gz
@@ -28,10 +35,10 @@ http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.t
 
 # export variables
 
-#rm -f variables.sh
-#touch variables.sh
+rm -f variables.sh
+touch variables.sh
 
-#echo export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_92 >> variables.sh
+echo export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_92 >> variables.sh
 # echo export PATH=$JAVA_HOME/bin:$PATH >> variables.sh
 
 #echo export PATH=/opt/apache-maven-3.3.9/bin:$PATH >> variables.sh
@@ -40,7 +47,5 @@ http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.t
 # echo export M2=$M2_HOME/bin >> variables.sh
 # echo export PATH=$M2:$PATH >> variables.sh
 
-#rm -f /etc/profile.d/variables.sh
-#cp variables.sh /etc/profile.d/
-
-
+rm -f /etc/profile.d/variables.sh
+cp variables.sh /etc/profile.d/
