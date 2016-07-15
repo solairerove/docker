424d9ab8fa3af585aec21293c79cd4f8781d0550
Krivitski Nikita <hax2033@gmail.com>
2016-07-16 00:42:51 +0300

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -126,3 +126,33 @@ pacman -S ibfonts-meta-extended-lt
 
 fc-presets set
 #3
+
+#<============>
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+wget --no-check-certificate --no-cookies --header \
+"Cookie: oraclelicense=accept-securebackup-cookie" \
+http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
+
+mkdir /usr/lib/jvm/
+tar -xf jdk-8u92-linux-x64.tar.gz -C /usr/lib/jvm/
+
+printf "${RED} <== install maven 3.3.9 ==> ${NC} \n"
+
+wget http://apache.osuosl.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+
+tar -xf apache-maven-3.3.9-bin.tar.gz -C /opt/
+
+printf "${RED} <== install tomcat 8.0.36 ==> ${NC} \n"
+
+wget --quiet --no-cookies \
+http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz
+
+tar -xf apache-tomcat-8.0.36.tar.gz -C /opt/
+rm -rf /opt/apache-tomcat-8.0.36/webapps/examples
+rm -rf /opt/apache-tomcat-8.0.36/webapps/docs
+
+chown -R $(whoami) /opt/apache-tomcat-8.0.36/
+chmod -R 777 /opt/apache-tomcat-8.0.36/
