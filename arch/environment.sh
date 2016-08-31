a14b9f2084ab9f18cc22d6eac50d6a5d05177ccf
root <root@localhost.localdomain>
2016-09-01 01:44:01 +0300

    Update environment script

--- arch/environment.sh
+++ arch/environment.sh
@@ -3,14 +3,14 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-printf "${RED} <== install jdk-8u92 ==> ${NC} \n"
+printf "${RED} <== install jdk-8u101 ==> ${NC} \n"
 
 wget --no-check-certificate --no-cookies --header \
 "Cookie: oraclelicense=accept-securebackup-cookie" \
-http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
+http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz
 
 mkdir /usr/lib/jvm/
-tar -xf jdk-8u92-linux-x64.tar.gz -C /usr/lib/jvm/
+tar -xf jdk-8u101-linux-x64.tar.gz -C /usr/lib/jvm/
 
 printf "${RED} <== install maven 3.3.9 ==> ${NC} \n"
 
@@ -30,12 +30,18 @@ rm -rf /opt/apache-tomcat-8.0.36/webapps/docs
 chown -R $(whoami) /opt/apache-tomcat-8.0.36/
 chmod -R 777 /opt/apache-tomcat-8.0.36/
 
-printf "${RED} <== install git ==> ${NC} \n"
+printf "${RED} <== install Intellij IDEA ==> ${NC} \n"
 
-GIT_NAME="krivitski-no"
-GIT_EMAIL="hax2033@gmail.com"
+wget https://download.jetbrains.com/idea/ideaIU-2016.2.3.tar.gz
 
-pacman -Sy git
+tar -xf ideaIU-2016.2.3.tar.gz -C ~/soft/
+#rm ideaIU-2016.1.3.tar.gz
 
-git config --global user.name "$GIT_NAME"
-git config --global user.email $GIT_EMAIL
+#chmod +x ~/soft/idea-IU-145.1617.8/bin/idea.sh
+#sed -i '2s/.*/-Xmx2048m/' ~/soft/idea-IU-145.1617.8/bin/idea64.vmoptions
+
+printf "${RED} <== export variables  ==> ${NC} \n"
+
+rm -f /etc/profile.d/variables.sh
+cp variables.sh /etc/profile.d/
+#rm -f variables.sh
---
+++ arch/variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_92
+export PATH=$JAVA_HOME/bin:$PATH
+
+export M2_HOME=/opt/apache-maven-3.3.9
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+
+export CATALINA_HOME=/opt/apache-tomcat-8.0.36
+export PATH=$CATALINA_HOME/bin:$PATH
+
+export JMETER_HOME=~/soft/apache-jmeter-3.0
+export PATH=$JMETER_HOME/bin:$PATH
