e49f41613ccd9306aab4311fb7ff35b6bb4c139c
solairerove <hax2033@gmail.com>
2016-09-12 23:08:17 +0300

    Ad gradle path variable

--- bash/variables.sh
+++ bash/variables.sh
@@ -7,5 +7,8 @@ export M2_HOME=/opt/apache-maven-3.3.9
 export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
 
+export GRADLE_HOME=/opt/gradle-3.0
+export PATH=$GRADLE_HOME/bin:$PATH
+
 export CATALINA_HOME=/opt/apache-tomcat-8.0.36
 export PATH=$CATALINA_HOME/bin:$PATH
