7bc65b0bbeac2001781df5933648901d80e29ab1
Krivitski Nikita <hax2033@gmail.com>
2016-11-16 12:24:40 +0300

    Increase maven max perm size

--- bash/variables.sh
+++ bash/variables.sh
@@ -2,12 +2,11 @@
 
 export JAVA_HOME=/opt/env/jdk
 export PATH=$JAVA_HOME/bin:$PATH
-export JAVA_OPTS="-XX:MaxPermSize=128M"
 
 export M2_HOME=/opt/env/maven
 export PATH=$PATH:$M2_HOME/bin
-export M2_OPTS="-Xmx1024M"
-export MAVEN_OPTS="-Xmx1024M"
+export M2_OPTS="-Xmx1024M -XX:MaxPermSize=256M"
+export MAVEN_OPTS="-Xmx1024M -XX:MaxPermSize=256M"
 
 export GRADLE_HOME=/opt/gradle-3.0
 export PATH=$GRADLE_HOME/bin:$PATH
