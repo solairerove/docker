6752919d84ba292a040b32fe17cf039d32876427
solairerove <hax2033@gmail.com>
2016-12-05 18:41:06 +0300

    Fix issue with cat

--- installation/kubernetes-ubuntu-server.sh
+++ installation/kubernetes-ubuntu-server.sh
@@ -2,7 +2,7 @@
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-at <<EOF > /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
