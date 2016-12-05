74289043bff0be91957bf4e28630cefd9b517bf7
solairerove <hax2033@gmail.com>
2016-12-05 18:22:37 +0300

    Add installation script of kubernetes on ubuntu server

---
+++ installation/kubernetes-ubuntu-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+at <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+kubeadm init
