8fa43248518709f00193ad0a18c3329a17eb5cb5
solairerove <hax2033@gmail.com>
2017-02-15 10:56:59 +0300

    Create tomcat jdk7 alpine based docker images

---
+++ tomcat/tomcat-jdk7-alpine/tomcat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+IMAGE_NAME=tomcat:jdk7
+CONTAINER_NAME=tomcat-jdk7
+
+if [ "$1" = "build"  ]
+then
+    echo "Build image"
+    docker build -t $IMAGE_NAME .
+elif [ "$1" = "run" ]
+then
+    echo "Run container"
+    docker run --name $CONTAINER_NAME -i -t -p 8989:8080 -d $IMAGE_NAME
+elif [ "$1" = "attach" ]
+then
+    docker attach $CONTAINER_NAME
+elif [ "$1" = "rm" ]
+then
+    docker rm -f $CONTAINER_NAME
+elif [ "$1" = "start" ]
+then
+    echo "Start container"
+    docker start $CONTAINER_NAME
+elif [ "$1" = "stop" ]
+then
+    echo "Stop container" docker stop $CONTAINER_NAME
+    docker stop $CONTAINER_NAME
+elif [ "$1" = "console" ]
+then
+    echo "Run console :"
+    docker exec -it $CONTAINER_NAME /bin/sh
+else
+    echo "Execute with parameter build|run|rm|start|stop|app_start|app_stop|console"
+fi
